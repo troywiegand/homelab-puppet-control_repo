ea20d07855f59ff3ca79f1a7a724be3e80bfaef9
Troy Wiegand <troy@troywiegand.com>
2020-02-17 11:00:04 -0500

    Use absolute path instead of puppet URI

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,6 +30,6 @@ node default {
   #   class { 'my_class': }
   class { 'motd': 
     dynamic_motd => false,
-    template     => "puppet://{$::hostname}.motd",
+    template     => "/home/troy/control-repo/files/{$::hostname}.motd",
   }
 }
